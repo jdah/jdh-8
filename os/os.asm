2b3b5983ec20b84a8b919c9e7a2cb136534ca2b8
jdah <20308439+jdah@users.noreply.github.com>
2022-01-05 08:17:37 +0100

    Fix bundled base OS

--- os/os.asm
+++ os/os.asm
@@ -1,37 +1,9 @@
 ; JDH-8 OPERATING SYSTEM
 ; INCLUDED IN OSTEXT.ASM
 
-@include "programs/holiday/image.asm"
-
 osmain:
   sw [ADDR_MB], 1
-  lda a, b, [ADDR_BANK]
-  lda c, d, [image]
-  lda [(SCANLINE_WIDTH_BYTES * SCREEN_HEIGHT)]
-  sw16 [D0], h, l
-  call [memcpy16]
+  sw16 [ADDR_SP], 0xFEFF
+  call [ADDR_RAM]
 .loop:
   jmp [.loop]
-
-  sw16 [ADDR_SPL], 0xFEFF
-  sw [ADDR_MB], 1
-
-  ; clear screen
-  lda a, b [ADDR_BANK]
-  lda c, d, (SCANLINE_WIDTH_BYTES * SCREEN_HEIGHT)
-  mw z, 0xFF
-  call [memset]
-
-  call [term_clear]
-
-  mw z, 24
-.loop0:
-  lda a, b, [string]
-  call [term_print_str]
-  dec z
-  jnz z, [.loop0]
-.loop1:
-  jmp [.loop1]
-
-string:
-  @ds "ABCDEFGHIJKLMNOPQRSTUVWXYZ\nZLKD\n"
